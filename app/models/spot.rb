e584046f8b9493a458566c28e5bca12556cc21b7
Lisa Ngo <lisango.e@gmail.com>
2019-08-19 18:17:04 +0200

    validations spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :user
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
