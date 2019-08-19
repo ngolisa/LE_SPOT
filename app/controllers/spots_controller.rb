107fdf55819b6914c6074897c067661f98b2a3c6
Lisa Ngo <lisango.e@gmail.com>
2019-08-19 16:33:25 +0200

    New et create de spot

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,4 +2,32 @@ class SpotsController < ApplicationController
   def index
     @spots = Spot.all
   end
+
+  def new
+    @spot = Spot.new
+  end
+
+  def create
+    @spot = Spot.new(spot_params)
+    @spot.user = current_user
+    if @spot.save
+      redirect_to spot_path(@spot)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+
+  end
+
+  def edit
+  end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:description, :price, :address, :city)
+  end
+
 end
